f6d4ad9983f28ad15fcc7e63eacfd333ae0a3dd7
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-10-24 20:51:42 -0600

    added strategy

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,1 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Personal website project</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Purpose</h2>
+
+<p>I would like my PWP to help me obtain the confidence I will need after this class by
+	applying the knowledge I’ve learned so far into building something from scratch that
+	can potentially be used by myself one day as my own business.</p>
+
+<h2>Audience</h2>
+
+<p>Busy bodies to have zero time in their day to worry about cooking their own meals or
+	having to take the time to go somewhere to pick something up.</p>
+
+<h2>Goal</h2>
+
+<p>To add to my gallery to showcase my work to eventually attract potential employers or clients. </p>
+
+<h1>Persona</h1>
+
+<img src="personapic.png"
+
+<h2>Name:</h2>
+<p>John Lawson</p>
+
+<h2>Age:</h2>
+<p>32</p>
+
+<h2>Profession:</h2>
+<p>Personal trainer at a local gym in Albuquerque, NM. Often works 7 days a week with odd hours.</p>
+
+<h2>Technology:</h2>
+<p>iphone user, often plays online video games using his Dell laptop that he carries with him. </p>
+
+<h2>Attitudes and Behaviors:</h2>
+<p>Always in a rush and on the move. Takes care of clients at different gyms and parks. Likes to take
+	advantage of his iphone to stay mobile but often has his laptop handy when he has spare time to play games.</p>
+
+<h2>Frustrations and Needs:</h2>
+<p>As mentioned John needs to be on the move constantly to make it to his next appointments with different
+	clients. He likes to take advantage of pre-made meals he can order online and have delivered to his location.
+	He finds when he has his meals prepared, he can grab and go. He has no time to cook for himself and ends up spending
+	too much money eating out. He gets upset that the only options are generally greasy fast food locations.</p>
+
+<h2>Goals:</h2>
+<p>John’s goal is to have a quick site to access to be able to place an order for food. He hopes the site will have
+	detailed descriptions of the various dishes available. As well as the cost on the food and delivery. </p>
+
+<h2>User Story:</h2>
+<p>As a quick user, John wants to be able to access a site and place a food order.</p>
+
+</body>
+</html>
